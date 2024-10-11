e260f240c3f2b846105079e27b65551e7ec650ce
Pyrrhus Elcid Go <pyrrhus1432@gmail.com>
2024-10-11 23:15:25 +0800

    Update CustomLabel.java

--- Prototype2/CustomLabel.java
+++ Prototype2/CustomLabel.java
@@ -7,30 +7,41 @@ import java.awt.event.MouseListener;
 
 public class CustomLabel extends JLabel implements MouseListener {
     String focusedDay = "";
-    public CustomLabel(String label/*,int horizontalAlignment*/){
+    public CustomLabel(String label){
         setText(label);
         setOpaque(true);
         setBackground(Color.white);
         addMouseListener(this);
         setFont(new Font("Calibre",Font.PLAIN,9));
-//        setHorizontalAlignment(horizontalAlignment);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        TemperatureManagement.ts.setVisible(true);
+
         TemperatureManagement.day= getText();
         TemperatureManagement.month = calendar.getMonth();
         TemperatureManagement.year = calendar.getYear();
         focusedDay = calendar.getMonth()+getText()+calendar.getYear();
         if(TemperatureManagement.getSchedTime.containsKey(focusedDay)){
-            SetTime.time.setText(String.valueOf(TemperatureManagement.getSchedTime.get(focusedDay)));
+            SetTime.time.setText(TemperatureManagement.getSchedTime.get(focusedDay)[0] + ":" + TemperatureManagement.getSchedTime.get(focusedDay)[1]);
+            SetTime.hour = TemperatureManagement.getSchedTime.get(focusedDay)[0];
+            SetTime.minute = TemperatureManagement.getSchedTime.get(focusedDay)[1];
         }
         if(TemperatureManagement.getTemp.containsKey(focusedDay)){
             TemperatureManagement.textField.setText(String.valueOf(TemperatureManagement.getTemp.get(focusedDay)));
+            SetTime.time.setText(TemperatureManagement.getSchedTime.get(focusedDay)[0] + ":" + TemperatureManagement.getSchedTime.get(focusedDay)[1]);
+            SetTime.hour = TemperatureManagement.getSchedTime.get(focusedDay)[0];
+            SetTime.minute = TemperatureManagement.getSchedTime.get(focusedDay)[1];
         }else {
-            TemperatureManagement.textField.setVisible(true);
+            SetTime.hour = 0;
+            SetTime.minute = 0;
+            SetTime.time.setText("00:00");
+            TemperatureManagement.textField.setText("");
         }
+        TemperatureManagement.textField.setVisible(true);
+        TemperatureManagement.set.setVisible(true);
+        TemperatureManagement.uni.setVisible(true);
+        TemperatureManagement.ts.setVisible(true);
     }
 
     @Override
